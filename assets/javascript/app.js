385b2a6270b3eeb5c72267bed3efc467a3f12c74
Hero's Dad <cody.monta@gmail.com>
2018-11-03 23:46:11 -0500

    here's what I got

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 var correct = 0;
 var incorrect = 0;
 var submitBtn = null;
-var seconds = 121;
+var seconds = 90;
 var intervalId = null;
 
 // for (var i = 0; i < questions.length; i++) {
@@ -28,7 +28,7 @@ var questions = [{
     correctAnswer: "b"
   },
   {
-    question: "In season 7, episode 17 Michael finally debuts his action film to the office after ten years of writing, schooting, re-shooting and editing. What is the name of this film?",
+    question: "In season 7, episode 17 Michael finally debuts his action film to the office after ten years of writing, shooting, re-shooting and editing. What is the name of this film?",
     answers: {
       a: "Code Red Threat",
       b: "Threat Level Midnight",
@@ -80,20 +80,50 @@ var questions = [{
   {
     question: "Which was NOT a prank that Jim successfully pulled on Dwight?",
     answers: {
-      a: "Classical conditioning Dwight to expect a mint at the sound of a computer log-in bell",
+      a: "Make Dwight to expect a mint at the sound of a computer log-in bell",
       b: "Jim impersonates Dwight by dressing up as a Dwight look-alike",
-      c: 'Tricking Dwight into trading a $150 telescope for "miracle legumes" ',
+      c: 'Tricked Dwight into trading a $150 telescope for "miracle legumes" ',
       d: "Stealing beets from Dwight's bed-and-breakfast and beet farm"
     },
     correctAnswer: "d"
   },
+  {
+    question: 'In S2E17 "Dwight\'s Speech": What infamous dictator\'s speech does Jim trick Dwight into giving at the sales conference?',
+    answers: {
+      a: "Adolf Hitler",
+      b: "Kim Jong II",
+      c: "Jospeh Stalin",
+      d: "Benito Mussolini"
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "What is the name of the award show that Michael hosts every year?",
+    answers: {
+      a: "Michael's Awards",
+      b: "The Dundies",
+      c: "Dunder Mifflin Grammys",
+      d: "The Dundie Show"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "In season 7, Toby is a jury member for what case?",
+    answers: {
+      a: "Scranton Strangler",
+      b: "Dunder Mifflin Building Not Up To Code",
+      c: "Ryan's Drug Problem",
+      d: "Printers Set Fire"
+    },
+    correctAnswer: "a"
+  },
 ]
 
-var getQuestions = function () {
-  for (var i = 0; i < questions.length ; i++) {
+var getQuestions = function() {
+  for (var i = 0; i < questions.length; i++) {
 
     // creates a new div for each answer
-    var newDiv = $(`<div id="question-format">${questions[i].question}</div>`)
+    var newDiv = $(`<div id="format-questions">${questions[i].question}</div>`)
     var newRadioBtnA = $(`<div id="format-answers"><input type="radio" name="question-${i}-option" id="AnswerA" value="a"><label id="choiceA" for="AnswerA">${questions[i].answers.a}</label></div>`)
     var newRadioBtnB = $(`<div id="format-answers"><input type="radio" name="question-${i}-option" id="AnswerB" value="b"><label id="choiceB" for="AnswerB">${questions[i].answers.b}</label></div>`)
     var newRadioBtnC = $(`<div id="format-answers"><input type="radio" name="question-${i}-option" id="AnswerC" value="c"><label id="choiceC" for="AnswerC">${questions[i].answers.c}</label></div>`)
@@ -111,15 +141,25 @@ var getQuestions = function () {
 }
 
 // function passing into the setInterval function argument
-var tick = function () {
-    seconds--;
-    $("#timeDiv").html(`<h2 class="time-format">Time Remaining: ${seconds} seconds</h2>`);
-    if (seconds == 0) {
-      clearInterval(intervalId);
-    }
+var tick = function() {
+  seconds--;
+  $("#timeDiv").html(`<h2 class="time-format">Time Remaining: ${seconds} seconds</h2>`);
+  if (seconds == 0) {
+    clearInterval(intervalId);
+    $("#timeDiv").hide()
+    $("#contentDiv").hide()
+    $("button").hide()
+    $("#times-up").css("display", "initial")
+    $("#times-up").html("<h1 class='no-time'>You've run out of time!</h2>")
+    $("#times-up-reset").show()
+    $("#times-up-reset").on("click", resetGame)
+
+
+  }
 }
 
-var timer = function (){
+
+var timer = function() {
   intervalId = setInterval(tick, 1000);
 }
 var submitBtn = null;
@@ -130,12 +170,13 @@ $("#startBtn").on("click", function() {
   $("#startBtn").hide();
   $("#start-btn-bg").hide()
   getQuestions()
-  submitBtn = $("#submit").html("<button>submit</button")
+  submitBtn = $("#submit").html("<button>submit</button>")
+  $(".add-a-class-questions").removeClass("h-auto").addClass("height-class-questions")
   showResults()
 })
 // submit button appears after start to submit the results of the trivia
-var showResults = function () {
-  $(submitBtn).on("click", function (){
+var showResults = function() {
+  $(submitBtn).on("click", function() {
     getResults()
     console.log("working")
     $("#contentDiv").hide()
@@ -146,6 +187,7 @@ var showResults = function () {
     $("#reset-game").on("click", resetGame)
     $(".rightResults").html(correct);
     $(".wrongResults").html(incorrect);
+    $(".add-a-class-results").removeClass("h-auto height-class-questions").addClass("height-class-results")
     clearInterval(intervalId);
 
 
@@ -154,28 +196,29 @@ var showResults = function () {
 }
 // reset game function - I can't figure out how to reset the questions so that none of the
 // radio buttons are selected
-var resetGame = function (){
-   $("input").prop('checked', false);
+var resetGame = function() {
+  $("input").prop('checked', false);
   $("#timeDiv").empty()
   $("#resultsDiv").hide();
   $("#timeDiv").show()
   $("#contentDiv").show()
   $("#submit").show()
-  seconds = 121;
+  $(".add-a-class-questions").removeClass("h-auto").addClass("height-class-questions")
+  seconds = 90;
   clearInterval(intervalId);
-timer()
+  timer()
 
 
 }
 
 
 var userAnswer = null;
-var getResults = function () {
+var getResults = function() {
   correct = 0;
   incorrect = 0;
-  for (var i = 0; i < questions.length ; i++) {
-  var currentQuestion = questions[i].correctAnswer;
-  userAnswer = $(`input[name=question-${i}-option]:checked`).val()
+  for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i].correctAnswer;
+    userAnswer = $(`input[name=question-${i}-option]:checked`).val()
 
     if (userAnswer === currentQuestion) {
       correct++;
@@ -187,22 +230,32 @@ var getResults = function () {
       incorrect++
       console.log(`incorrect count is ${incorrect}`);
 
-	  console.log("wrong!");
+      console.log("wrong!");
+    }
   }
 }
-}
 var coolHover = function() {
-  $("#startBtn").mouseover(function (){
-    $("#start-btn-bg").css({"opacity": "1","background":"#4484b9","height": "50%"})
-})
+  $("#startBtn").mouseover(function() {
+    $("#start-btn-bg").css({
+      "opacity": "1",
+      "background": "#4484b9",
+      "height": "50%"
+    })
+  })
 }
 var coolHoverTwo = function() {
-  $("#startBtn").mouseout(function (){
-    $("#start-btn-bg").css({"opacity": "0.9","background":"#4484b9","height": "50%"})
-})
+  $("#startBtn").mouseout(function() {
+    $("#start-btn-bg").css({
+      "opacity": "0.9",
+      "background": "#4484b9",
+      "height": "50%"
+    })
+  })
 }
-// shows the time remaining as user scrolls - I'm really proud of this one lol 
-window.onscroll = function() {myFunction()};
+// shows the time remaining as user scrolls - I'm really proud of this one lol
+window.onscroll = function() {
+  myFunction()
+};
 
 var header = document.getElementById("timeDiv");
 var sticky = header.offsetTop;
@@ -217,6 +270,6 @@ function myFunction() {
 
 
 // runs the reset game function when the play again button is clicked
-$("#reset-game").on("click",resetGame)
+$("#reset-game").on("click", resetGame)
 coolHover()
 coolHoverTwo()
